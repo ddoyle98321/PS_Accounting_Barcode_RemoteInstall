b6363de10a7909dc94cdb1dce5bfece8c9a0b31c
ddoyle98321 <ddoyle98321@gmail.com>
2017-02-22 14:25:32 -0800

    Version .1

---
+++ CopyFonts.ps1
@@ -0,0 +1,19 @@
+﻿
+# This is the file that contains the list of computers you want 
+# to copy the folder and files to. Change this path IAW your folder structure.
+$computers = gc "C:\Temp\computers.txt"
+ 
+# This is the directory you want to copy to the computer (IE. c:\folder_to_be_copied)
+$source = "\\dta01094p07\MSS\Private\MISCELLANEOUS\Font Install"
+ 
+# On the desination computer, where do you want the folder to be copied?
+$dest = "c$\Temp"
+ 
+foreach ($computer in $computers) {
+    if (test-Connection -Cn $computer -quiet) {
+        Copy-Item $source -Destination \\$computer\$dest -Recurse
+    } else {
+        Write-Output "$computer is not online" 
+    }
+ 
+}
---
+++ RunPSScript.ps1
@@ -0,0 +1,22 @@
+﻿
+# This is the file that contains the list of computers you want 
+# to copy the folder and files to. Change this path IAW your folder structure.
+$computers = gc "C:\Temp\computers.txt"
+ 
+# This is the directory you want to copy to the computer (IE. c:\folder_to_be_copied)
+$source = "c:\Temp\Font Install\Add-Font2.ps1"
+ 
+# On the desination computer, where do you want the folder to be copied?
+$dest = "C:\Temp\Font Install"
+$s = New-PSSession -ComputerName $computer
+$sb = "C:\Temp\Font Install\Add-Font2.ps1 -path C:\Temp\Font Install"
+
+foreach ($computer in $computers) {
+    if (test-Connection -Cn $computer -quiet) {
+        Invoke-Command -ComputerName $computer -ScriptBlock { & 'C:\Temp\Font Install\Add-Font2.ps1' -path "C:\Temp\Font Install" }
+        "Font installation on $computer complete"
+      } else {
+        "$computer is not online"
+    }
+ 
+}
